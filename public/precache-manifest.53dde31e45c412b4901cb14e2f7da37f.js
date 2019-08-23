50b07666b93bb56ce3c73b31196c18c72ce06ff4
Yaika <yaikazeeman@hotmail.com>
2019-08-23 15:07:17 +0200

    .

---
+++ public/precache-manifest.53dde31e45c412b4901cb14e2f7da37f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcda1bc1ff289770e041cee462377a73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42c1704b1f2e829cb677",
+    "url": "/static/css/main.55916e83.chunk.css"
+  },
+  {
+    "revision": "74dd514b6a50c333cbca",
+    "url": "/static/js/2.3e5d76f9.chunk.js"
+  },
+  {
+    "revision": "42c1704b1f2e829cb677",
+    "url": "/static/js/main.381dd1b1.chunk.js"
+  },
+  {
+    "revision": "81fdf8e34eec6a311ce7",
+    "url": "/static/js/runtime~main.57c249d2.js"
+  }
+]);
