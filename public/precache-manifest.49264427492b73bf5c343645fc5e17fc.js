7fd9528b3e26fe4fb7885d9ab89dffc46a3563e1
Yaika <yaikazeeman@hotmail.com>
2019-08-23 10:39:01 +0200

    update frontend

---
+++ public/precache-manifest.49264427492b73bf5c343645fc5e17fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb3d4ab4f674e59701fc0ee48cdf153a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9d3c0808de39efe27c6",
+    "url": "/static/css/main.55916e83.chunk.css"
+  },
+  {
+    "revision": "74dd514b6a50c333cbca",
+    "url": "/static/js/2.3e5d76f9.chunk.js"
+  },
+  {
+    "revision": "c9d3c0808de39efe27c6",
+    "url": "/static/js/main.1ded8a56.chunk.js"
+  },
+  {
+    "revision": "81fdf8e34eec6a311ce7",
+    "url": "/static/js/runtime~main.57c249d2.js"
+  }
+]);
